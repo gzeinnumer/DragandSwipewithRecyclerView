baba343468b7d6101265b3eb85ffea25a16bbb05
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-25 22:00:22 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/dragandswipewithrecyclerview/DataNo.java
@@ -0,0 +1,13 @@
+package com.gzeinnumer.dragandswipewithrecyclerview;
+
+class DataNo {
+    int no;
+
+    public DataNo(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+}
